16db67983d1d5d7e940b2fbce9912c82977c5a55
dhanujaya <sahandanujaya48@gmail.com>
2024-06-14 23:02:53 +0530

    git push

--- Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAO.java
+++ Layered-Architecture-Project/src/main/java/com/example/layeredarchitecture/dao/OrderDetailDAO.java
@@ -1,5 +1,12 @@
 package com.example.layeredarchitecture.dao;
 
-public interface OrderDetailDAO {
+import com.example.layeredarchitecture.model.OrderDetailDTO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public interface OrderDetailDAO {
+    public boolean saveDetail(Connection connection,String orderId, List<OrderDetailDTO> orderDetails) throws SQLException, ClassNotFoundException;
 }
